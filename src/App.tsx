7eddd0c885d1796d185d9a7f7ef2955fb40ae463
YumilRuedaFlores-Ksquare <Yumil.Flores@itksquare.edu.mx>
2022-12-29 20:26:32 +0100

    Task-19621 firebase

--- src/App.tsx
+++ src/App.tsx
@@ -1,23 +1,32 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import { getAuth, signInWithEmailAndPassword } from 'firebase/auth';
+import { firebaseConfig } from './firebase/firebaseConfig';
+import { initializeApp } from 'firebase/app';
+
+
+const firebaseApp = initializeApp(firebaseConfig);
+
+const auth = getAuth(firebaseApp);
 
 function App() {
+  const email = 'YumilwcTest2@gmail.com';
+  const passwd = 'yumil22';
+
+  const login = async () => {
+    try {
+      const user = await signInWithEmailAndPassword(auth, email, passwd);
+      console.log(user);
+    } catch (error) {
+      console.error(error);
+    }
+  }
+
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <button onClick={login} > Log in </button>
       </header>
     </div>
   );
