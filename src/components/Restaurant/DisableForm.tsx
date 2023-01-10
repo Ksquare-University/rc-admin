a5e4d549ae34c0b34148953a10f72b64a31f17b8
MarianaLopez-Ksquare <Mariana.Lopez@itksquare.edu.mx>
2023-01-10 14:05:56 -0600

    dissable

--- src/components/Restaurant/DisableForm.tsx
+++ src/components/Restaurant/DisableForm.tsx
@@ -5,20 +5,19 @@ import './DisableForm.css'
 export const DisableForm = () =>{
 
     return(
-        <div className="disableContainer">
+        <form className="disableContainer">
             <div className="restauranteAvility">
                 Enable/Disable
+                a
             </div>
             <div className="restauranteState">
                 Open/Close
             </div>
 
-        <div className="disableButtons">
-            <button className="cancelButton"> Cancel </button>
-    
-            <button className="doneButton"> Done </button>
-        </div>
-
-        </div>
+            <div className="disableButtons">
+                <button className="cancelButton"> Cancel </button>
+                <button className="doneButton"> Done </button>
+            </div>
+        </form>
     );
 }
