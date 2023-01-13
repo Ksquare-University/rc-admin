97d6375ebc8b0daf7d5dd803e48e220048b8278b
ArgenisGonzalez-Ksquare <argenis.gonzalez@itksquare.edu.mx>
2023-01-13 14:18:23 -0400

    Fixed Desings

--- src/App.tsx
+++ src/App.tsx
@@ -9,6 +9,7 @@ import { SeeYou } from "./components/SeeYou/SeeYou";
 import { UserInfo } from "./components/UserInfo/UserInfo";
 import { UserInfoSideBar } from "./components/UserInfoSideBar/UserInfoSideBar";
 import { UpNavBar } from "./components/UpNavBar/UpNavBar";
+import "./App.css"
 
 const firebaseApp = initializeApp(firebaseConfig);
 
@@ -29,7 +30,7 @@ function App() {
 
   return (
     <>
-      <UpNavBar />
+      <UserInfoSideBar/>
     </>
   );
 }
