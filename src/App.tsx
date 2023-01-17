67f8b67209937740b587cd73c91909d8f9ca9124
ArgenisGonzalez-Ksquare <argenis.gonzalez@itksquare.edu.mx>
2023-01-17 10:29:17 -0400

    changes

--- src/App.tsx
+++ src/App.tsx
@@ -1,18 +1,16 @@
 import React from "react";
 import logo from "./logo.svg";
-//import "./App.css";
+import "./App.css";
 import { Login } from "./components/Login/Login";
 import { SeeYou } from "./components/SeeYou/SeeYou";
 import { UserInfo } from "./components/UserInfo/UserInfo";
 import { UserInfoSideBar } from "./components/UserInfoSideBar/UserInfoSideBar";
 import { UpNavBar } from "./components/UpNavBar/UpNavBar";
 
-
 function App() {
-
   return (
     <>
-      <Login/>
+    <UpNavBar/>
     </>
   );
 }
