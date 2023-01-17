e3f30c748b9e544e545af810caf7fb876ec45a1c
ArgenisGonzalez-Ksquare <argenis.gonzalez@itksquare.edu.mx>
2023-01-17 14:44:04 -0400

    changes on css styles

--- src/views/RestaurantForm/index.tsx
+++ src/views/RestaurantForm/index.tsx
@@ -23,7 +23,7 @@ export function NewRestaurantForm ({ title = 'NewRestaurantForm' }: Props){
 
     const updateStates = (n: number) =>{
         dispatch(updateState(n));
-    }
+    
     
     return(
         <div className="formRestaurant">
@@ -40,4 +40,5 @@ export function NewRestaurantForm ({ title = 'NewRestaurantForm' }: Props){
             </div>
       </div>
     )
+}
 }
