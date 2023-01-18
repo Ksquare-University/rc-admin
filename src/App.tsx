bed48368e27e7abe42775ee0d18d0e4325a53c57
ArgenisGonzalez-Ksquare <argenis.gonzalez@itksquare.edu.mx>
2023-01-18 16:23:46 -0400

    Review

--- src/App.tsx
+++ src/App.tsx
@@ -1,36 +1,44 @@
-
-import { } from 'react-router-dom'
+import {} from "react-router-dom";
 import React, { useState, FC } from "react";
 import { Provider } from "react-redux";
-import Router from './Routes/index'
+import Router from "./Routes/index";
 import store from "./store";
 import { UserInfo } from "./components/UserInfo/UserInfo";
 import "./App.css";
 import { firebaseConfig } from "./firebase/firebaseConfig";
 import { initializeApp } from "firebase/app";
 import Sidebar from "./components/Sidebar/Sidebar";
-import { BrowserRouter, Routes } from 'react-router-dom';
-import { Login } from './components/Login/Login';
+import { BrowserRouter, Routes } from "react-router-dom";
+import { Login } from "./components/Login/Login";
+
+
 const firebaseApp = initializeApp(firebaseConfig);
-
-
 
 const App: FC = () => {
   const [open, setOpen] = useState<boolean>(false);
+
   const handleCallback = (childData: boolean) => {
     setOpen(childData);
   };
+
   const email = "YumilwcTest2@gmail.com";
   const passwd = "yumil22";
 
   // Set true if you want to see navBar and others routers componets
   // Set false if you want to go to login by default
-  const [isLogin, setLogin] = React.useState(false); 
+
+  const [isLogin, setLogin] = React.useState(true);
+
+  const parentLogin = (log: boolean) => {};
 
   return (
     <>
-      <Login/>
+      <BrowserRouter>
+          {/* Blocking routers if a user is not loggin */}
+          {isLogin ? <> <Sidebar /> <Router></Router></>: <Login></Login>}
+      </BrowserRouter>
     </>
   );
-};
+}
+
 export default App;
